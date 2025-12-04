e07fd0ee012573585c6a9dcb54e0c6edebf62d50
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2025-12-04 16:55:46 +0000

    changed

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 
 // Create the query client
 const queryClient = new QueryClient();
@@ -26,10 +26,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
                     <Sonner />
 
                     {/* This renders your entire application based on the router file */}
+                    <Analytics />
                     <RouterProvider router={router} />
                 </AuthContextProvider>
             </TooltipProvider>
         </QueryClientProvider>
     </React.StrictMode>
-    <Analytics />
 );
